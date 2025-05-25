c3d61fc6fccb0604696f330e5e03501e9b24e925
YounsooLim <tomandlim@gmail.com>
2025-05-25 18:09:34 +0900

    refactor: revert

--- assets/js/client.js
+++ assets/js/client.js
@@ -1,4 +1,5 @@
-let SUPABASE_URL;
-let SUPABASE_ANON_KEY;
+let SUPABASE_URL = "https://amssxykagnlmogalathk.supabase.co";
+let SUPABASE_ANON_KEY =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFtc3N4eWthZ25sbW9nYWxhdGhrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgxNTQzMjQsImV4cCI6MjA2MzczMDMyNH0.0SLp1Eh4-PRk7-ui-6_FKgKn9G21OjIj3FlSlsdTGOg";
 
 let client = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
